1bca56b68a27e46a37b396315f6d2e3a1a1fb1ac
Wolfsong <nemesidik@gmail.com>
2015-09-23 15:40:46 +0300

    test2
    test2

---
+++ SterNavigationTest/SterNavigationTest/SterNavigationTest/TeamPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace SterNavigationTest
+{
+    public partial class TeamPage
+    {
+        public TeamPage(HomePage mainPage)
+        {
+            InitializeComponent();
+            Parent = mainPage;
+        }
+
+        public new HomePage Parent { get; set; }
+
+       
+    }
+}
